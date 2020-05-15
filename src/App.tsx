12dcd04db2d2b486ce14d56e3ceea278416e0337
Jacob Frericks <jacob.frericks@gmail.com>
2020-05-15 11:02:08 -0500

    First pass at about me

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/NavbarScroller';
 import BrandBanner from './components/BrandBanner';
+import AboutMe from "./components/AboutMe";
 
 function App() {
     return (
     <div className="App">
       <Header/>
       <BrandBanner/>
+      <AboutMe/>
     </div>
   );
 }
