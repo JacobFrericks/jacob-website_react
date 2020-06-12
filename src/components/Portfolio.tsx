780c0025782eea9bc84e430653782bb155d2330a
Jacob Frericks <jacob.frericks@gmail.com>
2020-06-12 09:12:58 -0500

    Created portfolio (works on mobile)

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './Portfolio.scss';
 
-import styles from './Portfolio.module.css';
-
 import aca_eagles_banner from "../img/portfolio/aca_eagles.png";
 
 
 const Portfolio = () => {
     return (
-        <div>
-            <div className='title'> Projects</div>
-            <img src={aca_eagles_banner}/>
-        </div>
+        <section className="flex" id="portfolio">
+            <div className='title'>Projects</div>
+            <div className='projectGrid'>
+                <div className='test'/>
+                <div className='test1'/>
+                <div className='test2'/>
+                <div className='test3'/>
+                {/*<img className='project' src={aca_eagles_banner} alt='ACA Eagles'/>*/}
+                {/*<img className='project' src={aca_eagles_banner} alt='ACA Eagles'/>*/}
+                {/*<img className='project' src={aca_eagles_banner} alt='ACA Eagles'/>*/}
+                {/*<img className='project' src={aca_eagles_banner} alt='ACA Eagles'/>*/}
+            </div>
+        </section>
     );
         
 };
