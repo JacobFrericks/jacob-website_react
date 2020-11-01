cba8d14b1fa671b9861a41545a45860f4124813e
Jacob Frericks <JacobFrericks@users.noreply.github.com>
2020-11-01 17:16:14 -0600

    Adding pipeline (#27)
    Adding pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/NavbarScroller';
+// import Header from './components/NavbarScroller';
 import BrandBanner from './components/BrandBanner';
 import AboutMe from "./components/AboutMe";
 import Portfolio from "./components/Portfolio";
 import Blog from "./components/Blog";
-import WorkExperience from "./components/WorkExperience";
+// import WorkExperience from "./components/WorkExperience";
 import ContactMe from "./components/ContactMe";
 import Footer from "./components/Footer";
 
 function App() {
     return (
     <div className="App">
-      {/*<Header/>*/}
       <BrandBanner/>
       <AboutMe/>
       <Portfolio/>
       <Blog/>
-      {/*<WorkExperience/>*/}
       <ContactMe/>
       <Footer/>
     </div>
--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -5,30 +5,30 @@ import React from 'react';
 // import Facebook from '@material-ui/icons/Facebook';
 // import Twitter from '@material-ui/icons/Twitter';
 // import IconButton from '@material-ui/core/IconButton';
-import makeStyles from "@material-ui/core/styles/makeStyles";
+// import makeStyles from "@material-ui/core/styles/makeStyles";
 import './ContactMe.scss';
 
-let hover = { backgroundColor: "#fff", color: "#18bc9c", border: "solid 2px #fff" };
-let iconSize = { fontSize: '100px' };
+// let hover = { backgroundColor: "#fff", color: "#18bc9c", border: "solid 2px #fff" };
+// let iconSize = { fontSize: '100px' };
 
-const useStyles = makeStyles(theme => ({
-    linkedin: {
-        "&:hover, &.Mui-focusVisible": hover,
-        color: "#4875B4",
-    },
-    github: {
-        "&:hover, &.Mui-focusVisible": hover,
-        color: "#000"
-    },
-    facebook: {
-        "&:hover, &.Mui-focusVisible": hover,
-        color: "#3B5998"
-    },
-    twitter: {
-        "&:hover, &.Mui-focusVisible": hover,
-        color: "#33CCFF"
-    },
-}));
+// const useStyles = makeStyles(theme => ({
+//     linkedin: {
+//         "&:hover, &.Mui-focusVisible": hover,
+//         color: "#4875B4",
+//     },
+//     github: {
+//         "&:hover, &.Mui-focusVisible": hover,
+//         color: "#000"
+//     },
+//     facebook: {
+//         "&:hover, &.Mui-focusVisible": hover,
+//         color: "#3B5998"
+//     },
+//     twitter: {
+//         "&:hover, &.Mui-focusVisible": hover,
+//         color: "#33CCFF"
+//     },
+// }));
 
 const ContactMe = () => {
     return (
